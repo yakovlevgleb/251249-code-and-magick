e25e1d834db3d5803588a8dbacf782db8819f4e9
Gleb Yakovlev <brin@ya.ru>
2017-08-15 00:44:54 +0500

    fixes

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,28 @@
 'use strict';
 
 window.renderStatistics = function(ctx, names, times) {
+  var CANVAS_DATA = {
+    top: 20,
+    left: 110,
+    width: 420,
+    height: 270,
+    shadowOffset: 10,
+    textOffset: 20,
+    font: '16px PT Mono',
+    you = 'Вы',
+    win = 'Ура вы победили!',
+    resultList = 'Список результатов:'
+  }
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect(CANVAS_DATA.left, CANVAS_DATA.top, CANVAS_DATA.width, CANVAS_DATA.height);
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.fillRect(CANVAS_DATA.left - CANVAS_DATA.shadowOffset, CANVAS_DATA.top - CANVAS_DATA.shadowOffset, CANVAS_DATA.width, CANVAS_DATA.height);
 
   ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
+  ctx.font = CANVAS_DATA.font;
 
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText(CANVAS_DATA.win, CANVAS_DATA.left + CANVAS_DATA.shadowOffset, CANVAS_DATA.top + CANVAS_DATA.textOffset);
+  ctx.fillText(CANVAS_DATA.resultList, CANVAS_DATA.left + CANVAS_DATA.shadowOffset, CANVAS_DATA.top + 2*CANVAS_DATA.shadowOffset);
 
   var max = -1;
   var maxIndex = -1;
@@ -34,13 +46,13 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.textBaseline = 'top'; // Рисуем надпись от левого верхнего угла
   for (var i = 0; i < times.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.floor(times[i]), initialY + indent * i, -(times[i] * step) + initialX - 20);
+    ctx.fillText(Math.floor(times[i]), initialY + indent * i, -(times[i] * step) + initialX - CANVAS_DATA.textOffset);
     ctx.fillText(names[i], initialY + indent * i, initialX);
-    if (names[i] === 'Вы') {
+    if (names[i] === CANVAS_DATA.you) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1.0)';
     } else {
-      opac = Math.floor(Math.random() * 100)/100;
-      ctx.fillStyle = 'rgba(0, 0, 255,'+ opac +')';
+      opac = Math.floor(Math.random() * 100) / 100;
+      ctx.fillStyle = 'rgba(0, 0, 255,' + opac + ')';
     }
     ctx.fillRect(initialY + indent * i, initialX, barHeigth, -(times[i] * step));
   }
